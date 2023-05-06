86fef43dcc6e63a495fb11fff90ad968de4f2321
Godspower Udokpoh <harshmallins@gmail.com>
2023-05-06 09:53:01 -0700

    Web dynamics

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  const amenities = {};
+
+  $('input[type=checkbox]').change(function() {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if (this.checked) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    const amenitiesList = Object.values(amenities).join(', ');
+    $('.amenities h4').text(amenitiesList);
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+});
